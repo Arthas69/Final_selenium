5dcfbe39a672fc309656f093929f7d9c2e172474
Arthas69 <arthass36@gmail.com>
2020-12-08 16:30:41 +0300

    New MainPage class added, refactoring

---
+++ pages/main_page.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+
+    def __init__(self, browser, url):
+        super(MainPage, self).__init__(browser, url)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+
