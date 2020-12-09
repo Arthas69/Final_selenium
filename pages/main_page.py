594c0b88a66646925d39fcb7a844319c89aafa4c
Arthas69 <arthass36@gmail.com>
2020-12-09 10:02:39 +0300

    Login page test added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
+from .locators import MainPageLocator
 
 
 class MainPage(BasePage):
@@ -9,7 +8,9 @@ class MainPage(BasePage):
         super(MainPage, self).__init__(browser, url)
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocator.LOGIN_LINK)
         login_link.click()
+        return self.browser
 
-
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocator.LOGIN_LINK), 'Login link is not presented'
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,19 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
+
+
+def test_guest_can_open_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    browser = page.go_to_login_page()
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_page()
